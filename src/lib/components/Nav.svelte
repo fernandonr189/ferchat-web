b6b7e28e3ae9a8517d0e56944f5e7432e438e3aa
Fernando Navarrete <fernandonr189@outlook.com>
2024-11-16 13:15:28 -0600

    Changed svgs for fontawesome icons

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,32 +1,21 @@
 <script>
     import { Link } from "svelte-routing";
+    import Fa from "svelte-fa";
+    import { faBars, faEllipsis } from "@fortawesome/free-solid-svg-icons";
 </script>
 
 <div>
     <div class="sm:hidden">
         <div class="navbar bg-base-100">
             <div class="flex-none">
-                <!-- svelte-ignore a11y_consider_explicit_label -->
                 <div class="dropdown dropdown-bottom">
                     <div
                         tabindex="0"
                         role="button"
                         class="btn btn-ghost rounded-btn">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            class="inline-block h-5 w-5 stroke-current">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
+                        <Fa icon={faBars} />
                     </div>
-                    <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
                     <ul
-                        tabindex="0"
                         class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
                         <li>
                             <Link to="/">Home</Link>
@@ -41,20 +30,8 @@
                 <p class="btn btn-ghost text-xl">Ferchat</p>
             </div>
             <div class="flex-none">
-                <!-- svelte-ignore a11y_consider_explicit_label -->
                 <button class="btn btn-square btn-ghost">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        class="inline-block h-5 w-5 stroke-current">
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                        ></path>
-                    </svg>
+                    <Fa icon={faEllipsis} />
                 </button>
             </div>
         </div>
@@ -67,20 +44,8 @@
             </div>
             <div class="flex-none">
                 <li><a>Hello world</a></li>
-                <!-- svelte-ignore a11y_consider_explicit_label -->
                 <button class="btn btn-square btn-ghost">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        class="inline-block h-5 w-5 stroke-current">
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                        ></path>
-                    </svg>
+                    <Fa icon={faEllipsis} />
                 </button>
             </div>
         </div>

c72fe1babe64e04b0be72c3b2b1aac7512457338
Fernando Navarrete <fernandonr189@outlook.com>
2024-11-17 01:00:01 -0600

    reverted deletion of nav again :)

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { Router, Route } from "svelte-routing";
+    import { Router, Link, Route } from "svelte-routing";
     import Home from "./lib/routes/Home.svelte";
+    import Nav from "./lib/components/Nav.svelte";
     import About from "./lib/routes/About.svelte";
     import Signup from "./lib/routes/Signup.svelte";
     import Footer from "./lib/components/Footer.svelte";
@@ -10,7 +11,7 @@
 </script>
 
 <Router {url}>
-    
+    <Nav />
     <Route path="/" component={Home} />
     <Route path="/about" component={About} />
     <Route path="/signup" component={Signup} />
--- src/lib/routes/About.svelte
+++ src/lib/routes/About.svelte
@@ -1,10 +1,3 @@
-<script>
-    import Nav from "../components/Nav.svelte";
-</script>
-
-<div>
-    <Nav />
-    <div class="bg-slate-800 h-screen">
-        <p>This is the About page</p>
-    </div>
+<div class="bg-slate-800 h-screen">
+    <p>This is the About page</p>
 </div>

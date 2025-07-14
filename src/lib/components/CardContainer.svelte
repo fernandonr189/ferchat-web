9401ae1ead8ecf5331f7267c125b8321b3ca8db6
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2025-07-13 18:59:38 -0600

    Added hover on drawer for friends results

--- src/lib/components/CardContainer.svelte
+++ src/lib/components/CardContainer.svelte
@@ -3,26 +3,61 @@
     let { title, subtitle = null } = $props();
 </script>
 
-<div class="contact-container">
-    <img class="profile-pic" src={profilePic} alt="profile_picture" />
-    <div class="content">
-        <h4>{title}</h4>
-        {#if subtitle != null}
-            <p>{subtitle}</p>
-        {/if}
+<div class="top-container">
+    <div class="contact-container">
+        <img class="profile-pic" src={profilePic} alt="profile_picture" />
+        <div class="content">
+            <h4>{title}</h4>
+            {#if subtitle != null}
+                <p>{subtitle}</p>
+            {/if}
+        </div>
+    </div>
+    <div class="drawer">
+        <button>Send friend request</button>
+        <button>Block</button>
     </div>
 </div>
 
 <style>
+    .drawer {
+        display: flex;
+        flex-direction: column;
+        height: 6em;
+        box-sizing: border-box;
+    }
+    .drawer button {
+        background-color: #323333;
+        color: #fff;
+        border: none;
+        border-radius: 0em;
+        padding: 0em;
+        margin: 0em;
+        height: 2.5em;
+        box-sizing: border-box;
+    }
+    .top-container {
+        display: flex;
+        flex-direction: column;
+        height: 4.6em;
+        margin: 0.8em 0.8em 0 0.8em;
+        border-radius: 0.8em;
+        transition:
+            background-color 0.1s,
+            height 0.1s;
+        overflow: hidden;
+        box-sizing: border-box;
+    }
+    .top-container:hover {
+        height: 10.6em;
+    }
     .contact-container {
         display: flex;
         align-items: center;
         justify-content: start;
         height: 3em;
-        margin: 0.8em 0.8em 0 0.8em;
-        padding: 0.8em;
-        border-radius: 0.8em;
-        transition: background-color 0.1s;
+        margin: 0.8em;
+        box-sizing: border-box;
     }
     .content {
         display: flex;
@@ -30,7 +65,7 @@
         margin-left: 1em;
         text-align: start;
     }
-    .contact-container:hover {
+    .top-container:hover {
         background-color: #323333;
     }
     .contact-container h4 {
